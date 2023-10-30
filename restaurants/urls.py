b0cf8ad16884d89d391881254470094360a278fc
nayeongdev <nayeongkim.dev@gmail.com>
2023-10-30 10:46:54 +0900

    :sparkles: Feat: 추천식당 삭제하기
    - 추천식당 삭제 기능을 추가했습니다.
     - 추천식당 삭제 url을 생성했습니다.
     - 추천식당 삭제 확인 페이지를 만들었습니다.
     - 추천식당 삭제을 완료하면 글목록으로 넘어가도록 했습니다.
     - 추천식당 상세화면에 삭제 url을 넣었습니다.

--- restaurants/urls.py
+++ restaurants/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("<int:pk>/", views.restaurant_detail, name="restaurant-detail"),
     path("recommend/", views.restaurant_create, name="recommend"),
     path("<int:pk>/edit/", views.restaurant_update, name="restaurant-edit"),
+    path("<int:pk>/delete/", views.restaurant_delete, name="restaurant-delete"),
 ]
--- restaurants/views.py
+++ restaurants/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+)
 from .models import Restaurant
 from .forms import RestaurantForm
 
@@ -28,7 +34,13 @@ class RestaurantUpdate(UpdateView):
         return reverse_lazy("restaurant-detail", kwargs={"pk": self.object.pk})
 
 
+class RestaurantDelete(DeleteView):
+    model = Restaurant
+    success_url = reverse_lazy("restaurants")
+
+
 restaurant_list = RestaurantList.as_view()
 restaurant_detail = RestaurantDetail.as_view()
 restaurant_create = RestaurantCreate.as_view()
 restaurant_update = RestaurantUpdate.as_view()
+restaurant_delete = RestaurantDelete.as_view()
