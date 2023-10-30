a467ab773d461059011c4015f3f36a0561de52c3
nayeongdev <nayeongkim.dev@gmail.com>
2023-10-30 09:55:38 +0900

    :sparkles: Feat: 추천식당 등록하기
    - 추천식당 등록 기능을 추가했습니다.
    - 추천식당 등록 폼을 만들었습니다.
    - 추천식당 등록 url을 생성했습니다.
    - 추천식당 등록 화면을 구성했습니다.
    - 추천식당 등록 url과 연관되는 버튼에 연결했습니다.
    - 추천식당 등록 화면의 select 스타일을 추가했습니다.

--- restaurants/urls.py
+++ restaurants/urls.py
@@ -4,4 +4,5 @@ from restaurants import views
 urlpatterns = [
     path("", views.restaurant_list, name="restaurants"),
     path("<int:pk>/", views.restaurant_detail, name="restaurant-detail"),
+    path("recommend/", views.restaurant_create, name="recommend"),
 ]
--- restaurants/views.py
+++ restaurants/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Restaurant
+from .forms import RestaurantForm
 
 
 class RestaurantList(ListView):
@@ -12,5 +14,12 @@ class RestaurantDetail(DetailView):
     model = Restaurant
 
 
+class RestaurantCreate(CreateView):
+    model = Restaurant
+    form_class = RestaurantForm
+    success_url = reverse_lazy("restaurants")
+
+
 restaurant_list = RestaurantList.as_view()
 restaurant_detail = RestaurantDetail.as_view()
+restaurant_create = RestaurantCreate.as_view()
