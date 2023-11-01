3cb87ba0215b511f394aa3a816e3990240ef5d41
nayeongdev <nayeongkim.dev@gmail.com>
2023-11-01 14:38:58 +0900

    :sparkles: Feat: 회원가입 기능
    accounts앱에 회원가입 기능 추가
     - 회원가입 url 추가 (urls.py)
     - 회원가입 기능 추가 (views.py)
     - LOGIN_URL 추가 (settings.py)
     - 회원가입 폼 생성 (forms.py)
     - 회원가입 페이지 생성 (signup.html)
     - 회원가입 이동경로 추가 (login.html, base_generic.html)

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,8 @@
+from django.conf import settings
 from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic.edit import CreateView
 from django.urls import reverse_lazy
+from .forms import SignUpForm
 
 
 class UserLoginView(LoginView):
@@ -10,5 +13,13 @@ class UserLoginView(LoginView):
         return reverse_lazy("index")
 
 
+class SignUpView(CreateView):
+    form_class = SignUpForm
+    template_name = "accounts/signup.html"
+    success_url = settings.LOGIN_URL
+    permission_denied_message = "이미 가입한 사용자 입니다!"
+
+
 login = UserLoginView.as_view()
-logout = LogoutView.as_view(next_page="login")
+logout = LogoutView.as_view(next_page=settings.LOGIN_URL)
+signup = SignUpView.as_view()
