ea4bf5bd5398f4013a09c69382e4ae230b2c5d61
nayeongdev <nayeongkim.dev@gmail.com>
2023-11-01 15:12:33 +0900

    :sparkles: Feat: 회원가입 페이지 접근제한
    로그인한 사용자가 회원가입 페이지를 접근하는 것을 방지
    - SignUpView에 UserPassesTestMixin를 추가하여 기능 구현
    - 로그인한 사용자가 회원가입 페이지에 접근시, index로 redirect

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,10 @@
 from django.conf import settings
+from django.contrib.auth.mixins import UserPassesTestMixin
 from django.contrib.auth.views import LoginView, LogoutView
 from django.views.generic.edit import CreateView
+from django.shortcuts import redirect
 from django.urls import reverse_lazy
+
 from .forms import SignUpForm
 
 
@@ -13,11 +16,17 @@ class UserLoginView(LoginView):
         return reverse_lazy("index")
 
 
-class SignUpView(CreateView):
+class SignUpView(UserPassesTestMixin, CreateView):
     form_class = SignUpForm
     template_name = "accounts/signup.html"
     success_url = settings.LOGIN_URL
     permission_denied_message = "이미 가입한 사용자 입니다!"
+
+    def test_func(self):
+        return self.request.user.is_anonymous
+
+    def handle_no_permission(self):
+        return redirect("index")
 
 
 login = UserLoginView.as_view()
